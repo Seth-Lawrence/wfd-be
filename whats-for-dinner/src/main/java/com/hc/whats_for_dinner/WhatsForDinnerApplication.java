1e754001d5773e37f32ae275e5a0755213bd4cac
Seth Hobson <sethlawrencehobson@gmail.com>
2024-06-30 21:20:32 -0400

    initial commit

---
+++ whats-for-dinner/src/main/java/com/hc/whats_for_dinner/WhatsForDinnerApplication.java
@@ -0,0 +1,13 @@
+package com.hc.whats_for_dinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsForDinnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsForDinnerApplication.class, args);
+	}
+
+}
---
+++ whats-for-dinner/src/test/java/com/hc/whats_for_dinner/WhatsForDinnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc.whats_for_dinner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsForDinnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
